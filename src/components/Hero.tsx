d5231a95ea9842738e9fbf43261f0674f580cf3b
Luc Dao <daonguyenluc@gmail.com>
2024-02-18 21:45:30 -0500

    first commit

---
+++ src/components/Hero.tsx
@@ -0,0 +1,21 @@
+
+const Hero = () => {
+    return (
+        <section className="text-gray-600 body-font">
+            <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+                <div className="text-center lg:w-2/3 w-full">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Real-Time <br /> AI Voice Agent <br /> Designed for Your Business</h1>
+                    <p className="mb-8 leading-relaxed">
+                        Human-Like Calls - Always Available <br/>
+                        CRM Integration <br/>
+                        Crafted for Your Sales Playbooks <br/>
+                    </p>
+                    <div className="flex justify-center">
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
