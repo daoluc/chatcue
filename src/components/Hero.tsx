1dbd7c718f82ff158de60b2bcae4cb8720bd94fd
Luc Dao <daonguyenluc@gmail.com>
2024-03-01 14:55:50 -0500

    update slogan

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
         <section className="text-gray-600 body-font">
             <div className="container mx-auto flex px-3 py-1 items-center justify-center flex-col">
                 <div className="text-center lg:w-2/3 w-full">
-                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium"><a className="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-blue-400">Voice</a> AI <br /> Designed for <a className="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-blue-400">Your Business</a></h1>
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">Accessible <a className="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-blue-400">Voice</a> AI <br /> Designed for <a className="text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-blue-400">Your Business</a></h1>
                     <div className="leading-relaxed text-xl">
                         <b>Available 24/7</b> <br />
                         Increase Sales<br />
