1d8221e3db6ed63151aa0f7e2eadc5a8e52250b7
Luc Dao <daonguyenluc@gmail.com>
2024-03-10 17:24:58 -0400

    remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <main>
-      <Hero />
+      {/* <Hero />
       <Contact />
-      <Demo />
+      <Demo /> */}
     </main >
   );
 }
